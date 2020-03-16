98a1086cdb5fd5e76ffe55d202813df95f6a84c4
JLAT <jlat0128@gmail.com>
2020-03-15 21:43:21 -0400

    IT WORKS!

--- json_helper3.py
+++ json_helper3.py
@@ -5,20 +5,18 @@ import os
 
 # PART A
 def read_json(path):
-    with open(path) as f:
+    with open(path, 'r') as f:
         data = json.load(f)
         return data
 
 
 # PART B
 def read_all_json_files(path):
-    
     db_list = []
     
     for folder in os.listdir(path):
         files = os.path.join(path, folder)
         a = read_json(files)
-        
         db_list.append(eval(json.dumps(a)))
     
     return db_list
@@ -26,22 +24,33 @@ def read_all_json_files(path):
 
 # PART C
 
-def write_pickle(file_name, source):
-    
-    outfile = open(file_name, 'wb')  # (wb) write-binary mode
-    pickle.dump(source, file_name)
-    outfile.close()
+def write_pickle(data_source_path, output_name):
+    db = read_json(data_source_path)
+    f = open(output_name, 'wb')  # (wb) write-binary mode
+    pickle.dump(db, f)
+    f.close()
 
 
 # PART D
 def load_pickle(pickle_file):
-    open(pickle_file, 'rb')  # (rb)read-binary mode
-    db = pickle.load(pickle_file)
+    f = open(pickle_file, 'rb')  # (rb)read-binary mode
+    a = pickle.load(f)
+    db = (eval(json.dumps(a)))
     for keys in db:
         print(keys, '=>', db[keys])
-    pickle_file.close()
+    f.close()
+
+
+# Part D v2
+def loadpickle(filename):
+    f = open(filename, 'rb')
+    new_dict = pickle.load(f)
+    f.close()
+    return new_dict
+
 
 '''
 read_all_json_files('/Users/jthompson/dev/PythonFundamentals.Exercises.Part9/data/super_smash_bros/')
-write_pickle('/Users/jthompson/dev/PythonFundamentals.Exercises.Part9/data/super_smash_bros/mario.json/')
+write_pickle('/Users/jthompson/dev/PythonFundamentals.Exercises.Part9/data/super_smash_bros/mario.json', 'super_smash_characters.pickle')
+load_pickle('super_smash_characters.pickle')
 '''
